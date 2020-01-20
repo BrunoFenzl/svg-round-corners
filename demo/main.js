c29efe2dcfda780483e382845084ba23e23b2a1b
Bruno Fenzl <brunofenzl@gmail.com>
2020-01-20 21:26:00 +0100

    experimenting stuff

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,13 @@
-import { roundCorners } from '../lib';
+import { roundCorners, parsePath } from '../lib';
 
 const xmlns = "http://www.w3.org/2000/svg";
 
 const rangeSlider = document.getElementById('radius-range');
 
+const containers = [...document.querySelectorAll('svg')];
 const svgs = [...document.querySelectorAll('path')];
+
+const debug = document.getElementById('debug');
 
 svgs.forEach(svg => {
   const parent = svg.closest('svg');
@@ -15,6 +18,7 @@ svgs.forEach(svg => {
   
   svg.setAttribute('data-original-d', svg.getAttribute('d'));
   const rCorners = roundCorners(svg.getAttribute('data-original-d'), rangeSlider.value);
+  console.log(parsePath(svg.getAttribute('data-original-d')), rCorners);
   svg.setAttribute('d', rCorners.path);
   
   rCorners.commands.forEach((el, i) => {
@@ -22,7 +26,7 @@ svgs.forEach(svg => {
     circle.setAttributeNS(null, 'cx', el.values.x || 0);
     circle.setAttributeNS(null, 'cy', el.values.y || 0);
     // circle.setAttributeNS(null, 'r', el.radius);
-    parent.appendChild(circle);
+    // parent.appendChild(circle);
     // const txt = document.createElementNS(xmlns, "text");
     // txt.setAttributeNS(null, 'x', el.values.x || 0);
     // txt.setAttributeNS(null, 'y', el.values.y || 0);
@@ -31,7 +35,18 @@ svgs.forEach(svg => {
     if (el.degrees) {
     }
   });
-})
+
+  
+});
+
+containers.forEach(svg => {
+  const rect = svg.getBoundingClientRect();
+  svg.addEventListener('mousemove', (evt) => {
+    debug.innerText = `${evt.clientX - rect.left} ${evt.clientY - rect.top}`; 
+  });
+});
+
+
 
 rangeSlider.addEventListener('input', () => {
   svgs.forEach(svg => {
